33c8a60d1c8c9ec0cb3915664f12db373e0682c9
G4MB1T24 <65242995+G4MB1T24@users.noreply.github.com>
2022-07-18 22:02:57 +0530

    Real Commit

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -5,22 +5,10 @@ const { body, validationResult } = require("express-validator");
 
 router.post(
   "/createuser",
-  [
-    body("name").isLength({ min: 1 }).withMessage("Name is required"),
-    body("email").isEmail().withMessage("Email is required"),
-    body("password").isLength({ min: 5 }).withMessage("Password is required"),
-  ],
+
 
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      console.log(req.body);
-    } catch (error) {
-      res.send("error");
-    }
+    console.log(req.body.name)
   }
 );
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require("express")
-const connectToMongo = require("./db")
-const Auth = require("./Routes/Auth")
+const express = require("express");
+const connectToMongo = require("./db");
+const Auth = require("./Routes/Auth");
 var cors = require("cors");
-const app = express()
-const port = 5000
-connectToMongo()
+const app = express();
+const port = 5000;
+connectToMongo();
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth" ,Auth )
-app.listen(port, () => { console.log("listening") })
+app.use("/api/auth", Auth);
+app.listen(port, () => {
+  console.log("listening");
+});
