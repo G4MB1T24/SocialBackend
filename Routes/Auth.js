96a0c166fe33903e44f80f2905743801af6c809f
G4MB1T24 <65242995+G4MB1T24@users.noreply.github.com>
2022-07-20 00:06:13 +0530

    Create User

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -5,10 +5,36 @@ const { body, validationResult } = require("express-validator");
 
 router.post(
   "/createuser",
-
-
+  [
+    body("name").isLength({ min: 3 }).withMessage("Min 3 characters Required"),
+    body("email").isEmail().withMessage("Email Required"),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("Min 5 characters Required"),
+  ],
   async (req, res) => {
-    console.log(req.body.name)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let useremail = await Users.findOne({ email: req.body.email });
+      if (useremail) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      const User = await Users.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json({
+        User: User,
+      });
+    } catch (error) {
+      res.send("error");
+    }
   }
 );
 
